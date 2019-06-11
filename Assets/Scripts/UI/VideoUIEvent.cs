b80f9512368388490bfadae8ad7c18029140879b
kyns320 <kyn320@naver.com>
2019-06-12 03:01:49 +0900

    video scene work

---
+++ Assets/Scripts/UI/VideoUIEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoUIEvent : MonoBehaviour
+{
+    [SerializeField]
+    VideoPlayer videoPlayer;
+
+    bool played = false;
+
+    private void Update()
+    {
+        if (videoPlayer.isPrepared)
+            played = true;
+
+        if (played && !videoPlayer.isPlaying) {
+            OnSkip();
+        }
+    }
+
+    public void OnSkip()
+    {
+
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+}
