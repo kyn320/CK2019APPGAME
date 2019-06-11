54b5a269a0620ecbfd71da7793accd8f2c542544
kyns320 <kyn320@naver.com>
2019-06-11 23:39:50 +0900

    matching scene work

---
+++ Assets/Scripts/UI/MatchUIEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchUIEvent : MonoBehaviour
+{
+
+    public Sprite[] tipSprites;
+
+    public Image tipImage;
+
+    public Image matchingCircle;
+
+    float fillAmount = 0f;
+
+    private void Start()
+    {
+        tipImage.sprite = tipSprites[Random.Range(0, tipSprites.Length)];
+    }
+
+
+    private void Update()
+    {
+        fillAmount = Mathf.Abs(Mathf.Sin(Time.time));
+
+        matchingCircle.fillAmount = fillAmount;
+
+        if (fillAmount >= 0.99f)
+            matchingCircle.fillClockwise = false;
+
+        if (fillAmount <= 0.01f)
+            matchingCircle.fillClockwise = true;
+
+    }
+
+}
