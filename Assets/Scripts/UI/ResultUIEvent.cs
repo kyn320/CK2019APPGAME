bd39567bc03763578c591cbbe0ebccb28cf24404
kyns320 <kyn320@naver.com>
2019-06-11 23:08:28 +0900

    ui work

--- Assets/Scripts/UI/ResultUIEvent.cs
+++ Assets/Scripts/UI/ResultUIEvent.cs
@@ -2,9 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class ResultUIEvent : MonoBehaviour
 {
+    /*
+     제우스 = #7bbada
+     헤라 = #ff8484 
+     */
+
+    const string heraColorHex = "#FF8484";
+    const string zeusColorHex = "#7bbada";
+
+    public GameObject winTitle, loseTitle;
+
+    public TextMeshProUGUI buttonCountText;
+
+    public GameObject winEffect;
+    public Image winBackgroundImage;
+
+    public GameObject loseEffect;
+
+    public Image illustSlot;
+
+    public Sprite[] heraSprites, zeusSprites;
+
+
+    public void ViewResult()
+    {
+
+        int characterType = PlayerPrefs.GetInt("CharacterType", 0);
+        int GameResult = PlayerPrefs.GetInt("GameResult", 0);
+        int buttonCount = PlayerPrefs.GetInt("ButtonCount", 0);
+
+        if (characterType == 0)
+            GoToMain();
+
+        if (GameResult == 1)
+        {
+            winTitle.SetActive(true);
+            winEffect.SetActive(true);
+            Color color = new Color();
+            switch (characterType)
+            {
+                case 1:
+                    ColorUtility.TryParseHtmlString(zeusColorHex, out color);
+                    illustSlot.sprite = heraSprites[GameResult];
+                    break;
+                case 2:
+                    ColorUtility.TryParseHtmlString(heraColorHex, out color);
+                    illustSlot.sprite = zeusSprites[GameResult];
+                    break;
+            }
+            illustSlot.SetNativeSize();
+            winBackgroundImage.color = color;
+        }
+        else
+        {
+            switch (characterType)
+            {
+                case 1:
+                    illustSlot.sprite = heraSprites[GameResult];
+                    break;
+                case 2:
+                    illustSlot.sprite = zeusSprites[GameResult];
+                    break;
+            }
+
+            illustSlot.SetNativeSize();
+            loseTitle.SetActive(true);
+            loseEffect.SetActive(true);
+        }
+
+        buttonCountText.text = buttonCount.ToString();
+
+    }
+
     public void GameStart()
     {
         SceneManager.LoadScene("MatchingScene");
@@ -14,4 +88,7 @@ public class ResultUIEvent : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
+
+
+
 }
